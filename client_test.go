fdbbebf73a0772812ec00b7f20924271dfa19965
Fazlul Shahriar <fshahriar@gmail.com>
2009-11-26 13:10:05 -0500

    add more tests for Client.PlaylistInfo

--- client_test.go
+++ client_test.go
@@ -9,24 +9,43 @@ func localConnect(t *testing.T) (cli *Client) {
 	addr := "127.0.0.1:6600";
 	cli, err := Connect(addr);
 	if err != nil {
-		t.Fatalf("Connect(%q) = %v, %s want PTR, nil", addr, cli, err);
+		t.Fatalf("Connect(%q) = %v, %s want PTR, nil", addr, cli, err)
 	}
 	return;
+}
+
+func attrsEqual(left, right Attrs) bool {
+	if len(left) != len(right) {
+		return false
+	}
+	for key, lval := range left {
+		if rval, ok := right[key]; !ok || lval != rval {
+			return false
+		}
+	}
+	return true;
 }
 
 func TestPlaylistInfo(t *testing.T) {
 	cli := localConnect(t);
 	defer cli.Disconnect();
-	
+
 	pls, err := cli.PlaylistInfo(-1, -1);
 	if err != nil {
 		// We can't use t.Fatalf because defer'ed calls won't run
-		t.Errorf("Client.PlaylistInfo() = %v, %s need _, nil", pls, err);
+		t.Errorf("Client.PlaylistInfo(-1, -1) = %v, %s need _, nil", pls, err);
 		return;
 	}
 	for i, song := range pls {
 		if _, ok := song["file"]; !ok {
-			t.Errorf(`PlaylistInfo: song %d has no "file" attribute`, i);
+			t.Errorf(`PlaylistInfo: song %d has no "file" attribute`, i)
+		}
+		pls1, err := cli.PlaylistInfo(SongPOS(i), -1);
+		if err != nil {
+			t.Errorf("Client.PlaylistInfo(%d, -1) = %v, %s need _, nil", pls1, err)
+		}
+		if !attrsEqual(pls[i], pls1[0]) {
+			t.Errorf("Inconsistent song attribute for song %d", i)
 		}
 	}
 }
