055ef1055df27c4e3f1c24ec6a0ea2c9ac12cb53
Fazlul Shahriar <fshahriar@gmail.com>
2009-12-22 05:00:29 -0500

    add an example

---
+++ examples/status.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"mpd"
+	"os"
+	"time"
+)
+
+type Song struct {
+	title  string
+	artist string
+	album  string
+}
+
+func currentSong(cli *mpd.Client) (song *Song) {
+	song = new(Song)
+	sattr, err := cli.CurrentSong()
+	if err != nil {
+		return
+	}
+	song.title, _ = sattr["Title"]
+	song.artist, _ = sattr["Artist"]
+	song.album, _ = sattr["Album"]
+	return
+}
+
+func main() {
+	//mpd.Chatty = true;
+	cli, err := mpd.Connect("tcp", "127.0.0.1:6600")
+	if err != nil {
+		goto err
+	}
+	defer cli.Close()
+
+	line := ""
+	line1 := ""
+	for {
+		status, err := cli.Status()
+		if err != nil {
+			goto err
+		}
+		song := currentSong(cli)
+		if status["state"] == "play" {
+			line1 = fmt.Sprintf("%s - %s", song.artist, song.title)
+		} else {
+			line1 = fmt.Sprintf("State: %s", status["state"])
+		}
+		if line != line1 {
+			line = line1
+			fmt.Println(line)
+		}
+		time.Sleep(1e9)
+	}
+	return
+err:
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(2)
+}
