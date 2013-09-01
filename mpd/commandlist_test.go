9fb96525bab52e3b67e4efe2b8d85c803adc1bf2
Eric Butler <idealeric@gmail.com>
2013-09-01 13:51:06 -0400

    Add CommandList

---
+++ mpd/commandlist_test.go
@@ -0,0 +1,58 @@
+// Copyright 2009 The GoMPD Authors. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package mpd
+
+import (
+	"testing"
+)
+
+func TestCurrentSongPromise(t *testing.T) {
+	cli := localDial(t)
+	defer close(cli, t)
+
+	cmdl := cli.BeginCommandList()
+
+	pa := cmdl.CurrentSong()
+
+	if err := cmdl.End(); err != nil {
+		t.Errorf("CommandList.End failed: %s\n", err)
+	}
+
+	if _, err := pa.Value(); err != nil {
+		t.Errorf("Promise did not compute: %s\n", err)
+	}
+
+}
+
+func TestCommandList(t *testing.T) {
+	cli := localDial(t)
+	defer close(cli, t)
+
+	// Normal command list:
+	cmdl := cli.BeginCommandList()
+
+	cmdl.Next()
+	cmdl.Next()
+	cmdl.Next()
+
+	if err := cmdl.End(); err != nil {
+		t.Errorf("CommandList.End failed: %s\n", err)
+	}
+
+	// Test empty command list:
+	cmdl2 := cli.BeginCommandList()
+	if err := cmdl2.End(); err != nil {
+		t.Errorf("CommandList.End failed: %s\n", err)
+	}
+
+	// Reuse old comandlist (should work):
+	cmdl.Previous()
+	cmdl.Previous()
+	cmdl.Previous()
+	if err := cmdl.End(); err != nil {
+		t.Errorf("CommandList.End failed: %s\n", err)
+	}
+
+}
