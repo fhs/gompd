79a80a0a01b660586e67b2a228cd7bc77e8284c9
ushi <ushi@honkgong.info>
2013-09-25 02:01:18 +0200

    Added playlist related tests.

--- mpd/client_test.go
+++ mpd/client_test.go
@@ -114,3 +114,134 @@ func TestUpdate(t *testing.T) {
 		t.Errorf("job id is too small: %d\n", id)
 	}
 }
+
+func TestPlaylistFunctions(t *testing.T) {
+	cli := localDial(t)
+	defer close(cli, t)
+
+	files, err := cli.GetFiles()
+	if err != nil {
+		t.Errorf("Client.GetFiles failed: %s\n", err)
+		return
+	}
+	if len(files) < 2 {
+		return
+	}
+	for i := 0; i < 2; i++ {
+		if err = cli.PlaylistAdd("Test Playlist", files[i]); err != nil {
+			t.Errorf("Client.PlaylistAdd failed: %s\n", err)
+			return
+		}
+	}
+	attrs, err := cli.ListPlaylists()
+	if err != nil {
+		t.Errorf("Client.ListPlaylists failed: %s\n", err)
+		return
+	}
+	found := false
+	for _, pl := range attrs {
+		if pl["playlist"] == "Test Playlist" {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Errorf("Couldn't find playlist \"Test Playlist\" in %v\n", attrs)
+		return
+	}
+	attrs, err = cli.ListPlaylistInfo("Test Playlist")
+	if err != nil {
+		t.Errorf("Client.PlaylistInfo failed: %s\n", err)
+		return
+	}
+	found = false
+	for _, song := range attrs {
+		if song["file"] == files[0] {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Errorf("Couldn't find song \"%s\" in %v", attrs)
+		return
+	}
+	if err = cli.PlaylistDelete("Test Playlist", 0); err != nil {
+		t.Errorf("Client.PlaylistDelete failed: %s\n", err)
+		return
+	}
+	playlist, err := cli.ListPlaylistInfo("Test Playlist")
+	if err != nil {
+		t.Errorf("Client.ListPlaylistInfo failed: %s\n", err)
+		return
+	}
+	if len(playlist) != len(attrs)-1 {
+		t.Errorf("Unxpected number of tracks in the playlist: %d != %d", len(playlist), len(attrs))
+		return
+	}
+	if err = cli.Rename("Test Playlist", "Test Playlist 2"); err != nil {
+		t.Errorf("Client.Rename failed: %s\n", err)
+		return
+	}
+	if err = cli.Clear(); err != nil {
+		t.Errorf("Client.Clear failed: %s\n", err)
+		return
+	}
+	if err = cli.Load("Test Playlist 2", -1, -1); err != nil {
+		t.Errorf("Client.Load failed: %s\n", err)
+		return
+	}
+	attrs, err = cli.PlaylistInfo(-1, -1)
+	if err != nil {
+		t.Errorf("Client.PlaylistInfo failed: %s\n", err)
+		return
+	}
+	for i, attr := range attrs {
+		if attr["file"] != playlist[i]["file"] {
+			t.Errorf("Unexpected file: %s != %s\n", attr["files"], playlist[i]["file"])
+			return
+		}
+	}
+	if err = cli.PlaylistClear("Test Playlist 2"); err != nil {
+		t.Errorf("Client.Clear failed: %s\n", err)
+		return
+	}
+	attrs, err = cli.ListPlaylistInfo("Test Playlist 2")
+	if err != nil {
+		t.Errorf("Client.ListPlaylistInfo failed: %s\n", err)
+		return
+	}
+	if len(attrs) != 0 {
+		t.Errorf("Unexpected number of songs: %d != 0\n", len(attrs))
+		return
+	}
+	if err = cli.Rm("Test Playlist 2"); err != nil {
+		t.Errorf("Client.Rm failed: %s\n", err)
+		return
+	}
+	attrs, err = cli.ListPlaylists()
+	if err != nil {
+		t.Errorf("Client.ListPlaylists failed: %s\n", err)
+		return
+	}
+	for _, attr := range attrs {
+		if attr["playlist"] == "Test Playlist 2" {
+			t.Errorf("Found playlist \"Test Playlist 2\" in %v\n", attrs)
+			return
+		}
+	}
+	if err = cli.Save("Test Playlist"); err != nil {
+		t.Errorf("Client.Save failed: %s\n", err)
+		return
+	}
+	attrs, err = cli.ListPlaylistInfo("Test Playlist")
+	if err != nil {
+		t.Errorf("Client.ListPlaylistInfo failed: %s\n", err)
+		return
+	}
+	for i, attr := range attrs {
+		if attr["file"] != playlist[i]["file"] {
+			t.Errorf("Unexpected file: %s != %s\n", attr["files"], playlist[i]["file"])
+			return
+		}
+	}
+}
