2ff632109cba026138d36cfd246409113fd1154e
Fazlul Shahriar <fshahriar@gmail.com>
2018-01-15 22:43:07 -0500

    test: correct bad format verb

--- mpd/client_test.go
+++ mpd/client_test.go
@@ -233,7 +233,7 @@ func TestListOutputs(t *testing.T) {
 		}
 		for k, v := range expected[i] {
 			if outputs[i][k] != v {
-				t.Errorf(`Expected property %o for key "%s", got %o`, v, k, outputs[i][k])
+				t.Errorf(`Expected property %s for key "%s", got %s`, v, k, outputs[i][k])
 			}
 		}
 	}
